78f7bb7cb97429688d79d35b698b75fd76c3391d
gayfulina <irinmdr@gmail.com>
2021-08-06 09:46:15 -0700

    server3

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,40 @@
+const aws = require("aws-sdk");
+const multer = require('multer');
+const multerS3 = require('multer-s3')
 
-exports.setProfilePic = (req,res,next) => {
-    console.log(req.files);
+const s3 = new aws.S3({
+    accessKey: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION,
+})
 
-    res.status(200).json({data: req.files});
+const upload = (bucketName) =>
+    multer({
+        storage: multerS3({
+            s3,
+            bucket: bucketName,
+            metadata: function (req, file, cb) {
+                cb(null, { fieldName: file.fieldname });
+            },
+            key: function (req, file, cb) {
+                cb(null, `image-${Date.now()}.jpeg`);
+            },
+        }),
+    });
+
+
+
+exports.setProfilePic = (req, res, next) => {
+    const uploadSingle = upload("profile-picture-upload-test").single(
+        "croppedImage"
+    );
+
+    uploadSingle(req, res, async (err) => {
+        if (err)
+            return res.status(400).json({ success: false, message: err.message });
+
+        console.log(req.file)
+
+        res.status(200).json({ data: req.file.location });
+    })
 }
--- server/index.js
+++ server/index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-const expressfileupload = require('express-fileupload')
 
 const app = express();
 
 const userRouter = require('./routes/userRoutes');
 
-app.use(cors(), express.json(), expressfileupload());
+app.use(cors(), express.json());
 
 app.get('/', (req, res) =>
     res.json({success: true, message: 'Server is running'})
